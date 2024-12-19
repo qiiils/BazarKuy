af5ca4ceb59495626ceee5fdc2fd8bec7bc0aa3e
qiiils <001qils@gmail.com>
2024-12-19 16:41:11 +0700

    data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        buildConfigField("String", "BASE_URL","\"https://8shnf50m-3000.asse.devtunnels.ms/\"")
+        buildConfigField("String", "BASE_URL", "\"http://10.44.10.107:3000/\"")
         vectorDrawables {
             useSupportLibrary = true
         }
